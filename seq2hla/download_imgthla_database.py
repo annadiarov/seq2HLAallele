da42f658201df6a4b449b0909fa2a3206297b6fe
annadiarov <annadiarov@gmail.com>
2023-10-12 23:54:17 +0200

    feat(databases): Create Blast DB after downloading files

--- seq2hla/download_imgthla_database.py
+++ seq2hla/download_imgthla_database.py
@@ -1,5 +1,6 @@
 import os
 import requests
+import subprocess
 
 MOLECULE_TYPE = 'prot'
 ALL_LOCI = ['A', 'B', 'C', 'DPA1', 'DPB1', 'DQA1', 'DQB1', 'DRB1']
@@ -33,6 +34,23 @@ def merge_hla_seq(output_dir, output_file):
                     out_file.write(line)
 
 
+def make_blast_db(input_file):
+    cmd = [
+        'makeblastdb',
+        '-in',
+        input_file,
+        '-dbtype',
+        'prot',
+        '-parse_seqids'
+    ]
+
+    try:
+        subprocess.run(cmd, check=True)
+        print(f"makeblastdb command executed successfully for {input_file}.")
+    except subprocess.CalledProcessError as e:
+        print(f"An error occurred while executing makeblastdb: {e}")
+
+
 if __name__ == "__main__":
     this_file_path = os.path.dirname(os.path.abspath(__file__))
     databases_dir = os.path.join(this_file_path, '..', 'databases')
@@ -42,4 +60,6 @@ if __name__ == "__main__":
     all_hla_seq_file = os.path.join(output_dir, 'all_hla_seq.fasta')
     get_hla_seq_latest_version(output_dir)
     merge_hla_seq(output_dir, all_hla_seq_file)
-    print('Successfully downloaded and merged HLA sequences! \^o^/')
+    make_blast_db(all_hla_seq_file)
+    print('Successfully downloaded, merged and created a Blast DB of '
+          'HLA sequences! \^o^/')
